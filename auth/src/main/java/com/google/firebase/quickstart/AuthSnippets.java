152bd7b0a846019c4e9c106d5ef754d0e7b8c158
Avishalom Shalit <avishalom@gmail.com>
2018-02-14 18:17:51 -0500

    .

--- auth/src/main/java/com/google/firebase/quickstart/AuthSnippets.java
+++ auth/src/main/java/com/google/firebase/quickstart/AuthSnippets.java
@@ -1,7 +1,6 @@
 package com.google.firebase.quickstart;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.common.collect.ImmutableMap;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.ExportedUserRecord;
